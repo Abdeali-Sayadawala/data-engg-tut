cc6f485d26c2e1f8e509c5cf8c9cc6cf05c6c075
Abdeali Sayadawala <abdeali009@gmail.com>
2023-11-11 23:05:13 +0530

    Update ghactivity-downloader.tf

--- ghactivity-downloader.tf
+++ ghactivity-downloader.tf
@@ -11,7 +11,7 @@ resource "aws_lambda_function" "ghactivity_downloader_lambda" {
     function_name = "${local.function_name}-function"
     role = "arn:aws:iam::256772913192:role/ghactivity-downloader-role"
     handler = "ghactivity-downloader.lambda_handler"
-    s3_bucket = data.aws_s3_bucket_object.scripts_bucket
+    s3_bucket = data.aws_s3_bucket_object.scripts_bucket.bucket
     s3_key = data.aws_s3_bucket_object.scripts_bucket.key
     runtime = "python3.10"
     timeout = "800"
